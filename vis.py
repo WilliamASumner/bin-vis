dd4d187315887f3f99d42a738d3ab3356ad09d2c
Will Sumner <williamasumner@gmail.com>
2022-03-07 10:51:48 -0500

    Initial commit

---
+++ vis.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import vaex
+import ipyvolume as ipv
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import numpy as np
+import argparse, sys
+
+def getFileData(file):
+    with open(file,"rb") as binFile:
+        fileBytes = list(binFile.read())
+    return fileBytes
+
+def genCoords(data, dims = 2):
+    data = np.asarray(data,dtype=np.ubyte)
+    return np.lib.stride_tricks.sliding_window_view(data,dims,axis=0)
+
+
+def genNdGrid(coords, dims = 2):
+    grid = np.zeros((256,) * dims)
+    for coord in coords:
+        grid[tuple(coord)] += 1
+    return grid
+
+def mplPlot(data):
+    plt.imshow(data, interpolation = 'none',cmap='hot',norm = LogNorm(), origin = "lower")
+    ax = plt.gca()
+    ax.set_facecolor("black")
+    plt.show()
+
+def vaexPlot(data, dims = 2):
+    df = vaex.from_arrays(d=data)
+    if dims == 2:
+        df.viz.heatmap(df.d[:,0],df.d[:,1], limits="99.7%", what = [np.log(vaex.stat.count()+1)])
+        plt.show()
+    elif dims == 3:
+        df.plot_widget(df.d[:,0], df.d[:,1], df.d[:,2], what = [np.log(vaex.stat.count()+1)] )
+        #ipv.quickscatter(df.d[:,0],df.d[:,1],df.d[:,2], size = 1, marker = "sphere")
+
+def parseArgs(argList):
+    args = argparse.ArgumentParser(description="Binary visualizer")
+    args.add_argument(metavar="File",dest='files',nargs='+',help="Files to plot")
+    args.add_argument("--dimensions","-d",type=int,default=2,choices=[2,3],help="Number of dimensions to visualize binary in")
+    args.add_argument("--mode","-m",default="mpl",choices=["matplotlib","mpl","vaex","v"],help="Visualization mode/library to use")
+    args = args.parse_args(argList)
+    return args
+
+
+def main(**args):
+    dims = args.get('dimensions')
+    for file in args.get('files'):
+        fileBytes = getFileData(file)
+        coords = genCoords(fileBytes,dims)
+        if args.get('mode') in ['mpl','matplotlib']:
+            grid = genNdGrid(coords, dims)
+            mplPlot(grid)
+        else:
+            vaexPlot(coords, dims)
+
+if __name__ == '__main__':
+    args = parseArgs(sys.argv[1::])
+    main(**vars(args))
