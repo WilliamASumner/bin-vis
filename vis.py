a5adc82e3939aef344f55f643f74f581dd33fc51
Will Sumner <williamasumner@gmail.com>
2022-03-08 09:24:42 -0500

    Try to sync mpl and vaex outputs, add ability to generate raw imgs

--- vis.py
+++ vis.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import numpy as np
 import argparse, sys, os
+import warnings
 
 def getFileData(file):
     with open(file,"rb") as binFile:
@@ -16,24 +17,51 @@ def genCoords(data, dims = 2):
     return np.lib.stride_tricks.sliding_window_view(data,dims,axis=0)
 
 def genNdGrid(coords, dims = 2):
-    grid = np.zeros((256,) * dims)
+    grid = np.zeros((256,) * dims,dtype=np.ubyte)
     for coord in coords:
         grid[tuple(coord)] += 1
     return grid
 
 def mplPlot(data):
-    plt.imshow(data, interpolation = 'none',cmap='hot',norm = LogNorm(), origin = "lower")
+    assert np.all(np.isfinite(data))
+    im = plt.pcolormesh(data, cmap='hot',norm = LogNorm(), shading = "flat")
+    cbar = plt.colorbar(im)
+    cbar.set_label("Log(count)")
     ax = plt.gca()
     ax.set_facecolor("black")
 
-def vaexPlot(data, dims = 2):
+def vaexPlot(data, dims = 2, isOutputFile = True):
     df = vaex.from_arrays(d=data)
+    lim = "100%" if isOutputFile else "99%"
     if dims == 2:
-        df.viz.heatmap(df.d[:,0],df.d[:,1], limits="99%", what = [np.log(vaex.stat.count()+1)])
+        hm = df.viz.heatmap(df.d[:,0],df.d[:,1], limits=lim, what = [np.log(vaex.stat.count()+1)], colorbar = isOutputFile)
     elif dims == 3:
         raise NotImplementedError("No 3D support from vaex yet")
         #df.plot_widget(df.d[:,0], df.d[:,1], df.d[:,2], what = [np.log(vaex.stat.count()+1)] )
         #ipv.quickscatter(df.d[:,0],df.d[:,1],df.d[:,2], size = 1, marker = "sphere")
+    else:
+        raise ValueError("Too many dimensions")
+
+def streamlinePlot():
+    ax = plt.gca()
+    im = ax.collections
+    for i in im:
+        cb = i.colorbar
+        if cb is None:
+            continue
+        cb.remove()
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.tick_params(left=False, bottom=False,
+                   labelleft=False,labelbottom=False)
+    fig = ax.get_figure()
+    fig.tight_layout()
+    fig.subplots_adjust(top=1.0,
+                        bottom= 0.0,
+                        left = 0.0,
+                        right = 1.0)
+    fig.set_size_inches(4,4,forward=True)
+
 
 def beautifyPlot(file):
     ax = plt.gca()
@@ -50,6 +78,7 @@ def parseArgs(argList):
     args.add_argument(metavar="File",dest='files',nargs='+',help="Files to plot")
     args.add_argument("--dimensions","-d",type=int,default=2,choices=[2,3],help="Number of dimensions to visualize binary in")
     args.add_argument("--mode","-m",default="mpl",choices=["matplotlib","mpl","vaex","v"],help="Visualization mode/library to use")
+    args.add_argument("--outputfile","-o",help="File to output image to")
     args = args.parse_args(argList)
     return args
 
@@ -59,13 +88,23 @@ def main(**args):
     for file in args.get('files'):
         fileBytes = getFileData(file)
         coords = genCoords(fileBytes,dims)
+        displayingResults = args.get('outputfile') is None
         if args.get('mode') in ['mpl','matplotlib']:
             grid = genNdGrid(coords, dims)
             mplPlot(grid)
         else:
-            vaexPlot(coords, dims)
-        beautifyPlot(file)
-        plt.show()
+            vaexPlot(coords, dims, displayingResults)
+
+        if displayingResults:
+            beautifyPlot(file)
+            plt.show()
+        else:
+            streamlinePlot()
+            #plt.show()
+            plt.savefig(args.get('outputfile'),
+                        bbox_inches='tight',
+                        dpi = 64,
+                        pad_inches=0)
 
 if __name__ == '__main__':
     args = parseArgs(sys.argv[1::])
